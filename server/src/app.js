0e45ee2ed20cb473bc7c02b73065f28526256f77
Diogo Machado <diogomachado_8@hotmail.com>
2019-09-29 08:02:03 -0300

    Feat: Server add auth's routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,15 @@
-import 'dotenv/config';
+import './bootstrap';
 
 import Youch from 'youch';
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 
 // Uncomment this line to enable database access
 // --------
-// import './database';
+import './database';
 
 class App {
   constructor() {
@@ -20,6 +21,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
+routes.post('/users', UserController.store);
+routes.get('/', (req, res) => res.send('ok Test'));
+
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
